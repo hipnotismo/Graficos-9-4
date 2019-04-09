09227e73b6b3c29d9602082572f2a2173f4f7f72
Martin <martinconcetti97@gmail.com>
2019-04-09 11:40:17 -0300

    Primeras pruebas
    Falta pulido

---
+++ Lib-hightScore/Lib-hightScore/Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "HightScore.h"
+
+int HightScore(int score) {
+	int HighScore = 0;
+	int NewScore = score;
+	if (HighScore < NewScore)
+	{
+		HighScore == NewScore;
+	}
+	return HighScore;
+}
